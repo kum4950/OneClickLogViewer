8d10fb428f685b9e85b9f3714e2a26cef4e36fce
kumsungwon <kumsungwon1@gmail.com>
2023-05-04 04:08:09 +0900

    WPF Modern UI

---
+++ ModernUI_practice/ModernUI_practice/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ModernUI_practice.Core;
+
+namespace ModernUI_practice.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object? _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView!; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
